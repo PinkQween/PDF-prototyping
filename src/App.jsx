3b29a653819eb2970582e0787c39fa7963a1ba01
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-04-10 14:40:20 -0600

    Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <p>enter PDF size (in):</p>
+      <input type="number" />
+      <input type="number" />
+      <button type="submit">Enter</button>
+    </>
+  )
+}
+
+export default App
